a132a123eddc5855f7cbecdf9eb9e7d5e8510625
shivani <shivanirm8119@gmail.com>
2024-04-02 16:13:45 +0530

    added

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD7-KPSQVBi7cXGV3PivSUGuRWorDvDEe4",
+    authDomain: "codebin-ng-e9184.firebaseapp.com",
+    projectId: "codebin-ng-e9184",
+    storageBucket: "codebin-ng-e9184.appspot.com",
+    messagingSenderId: "26023778469",
+    appId: "1:26023778469:web:0d2840569c9ae0ff7d554c",
+    measurementId: "G-TWXB2QHWNV"
+  };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import{firebaseConfig} from "../../firebaseConfig";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent]
 })
 export class AppComponent {
-  title = 'codebin';
+constructor(){
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+}
 }
